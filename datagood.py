5a68bdc9a77bd15e1c96f43517354f8405fafd99
extr0welt <96325783+extr0welt@users.noreply.github.com>
2022-12-22 19:45:32 +1000

    updated
    move block 'with open'(from datagood.py) in to 'main.py'

--- datagood.py
+++ datagood.py
@@ -114,14 +114,3 @@ class GoodList:
 
 good_list = GoodList()
 
-with open('list_goods.txt', 'r', encoding='utf-8') as file:
-    list_with_goods = file.readlines()
-
-    for str_good in list_with_goods:
-        list_good = str_good.split(':')
-        name = list_good[0]
-        price = list_good[1]
-        count = list_good[2]
-        production_date = list_good[3]
-        expiration_day = list_good[4]
-        good_list.add_good_in_list(Good(name, price, count, production_date, expiration_day))
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import datagood
 
+with open('list_goods.txt', 'r', encoding='utf-8') as file:
+    list_with_goods = file.readlines()
+
+    for str_good in list_with_goods:
+        list_good = str_good.split(':')
+        name = list_good[0]
+        price = list_good[1]
+        count = list_good[2]
+        production_date = list_good[3]
+        expiration_day = list_good[4]
+        datagood.good_list.add_good_in_list(datagood.Good(name, price, count, production_date, expiration_day))
+
 if __name__ == '__main__':
     print('Товар с максимальной ценой:', datagood.good_list.get_good_with_max_price())
     print('Товар с минимальной ценой:', datagood.good_list.get_good_with_min_price())
